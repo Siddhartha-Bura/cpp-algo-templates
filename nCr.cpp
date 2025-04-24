0b36f6a89b4834d2998827bc275e72d0fea5affd
Siddhartha-Bura <44285622+Siddhartha-Bura@users.noreply.github.com>
2025-04-24 23:03:33 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -11,3 +11,25 @@ int nCr(int n, int r) {
 // Credits: gfg
 // TC: O(r)
 // SC: O(1)
+
+int factMemo[100000] = {};
+const int mod = 1e9+7;
+ll power(ll a, ll b, ll m) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a) % m;
+        a = (a * a) % m;
+        b = b >> 1;
+    } 
+    return res;
+}
+ll fact(ll n) {
+    if (n == 0) return 1;
+    if (factMemo[n]) return factMemo[n];
+    factMemo[n] = (n * fact(n - 1)) % mod;
+    return factMemo[n];
+}
+ll nCr(ll a, ll b) {
+    return (((fact(a) * power(fact(b), mod - 2, mod)) % mod) * power(fact(a - b), mod - 2, mod)) % mod;
+}
